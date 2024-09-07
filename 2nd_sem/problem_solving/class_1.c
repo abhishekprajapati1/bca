6a907010eea79d531ab05cabac206e5a0a15a62f
abhishekprajapati1 <ap0661236@gmail.com>
2024-09-07 10:16:29 +0530

    class_1 added

--- 2nd_sem/problem_solving/class_1.c
+++ 2nd_sem/problem_solving/class_1.c
@@ -1,21 +1,28 @@
+// date: Sep 7, 2024
 #include <stdio.h>
 
-int main() {
-   // Linear search
-   int array[5] = {5, 1, 2, 3, 4};
-   int target = 4; // worst case
-   int loc = -1;
+int main()
+{
+    // Linear search
+    int array[5] = {5, 1, 2, 3, 4};
+    int target = 4; // worst case
+    int loc = -1;
     int i = 0;
-    while(i <= 4) {
-        if(array[i] == target) {
+    while (i <= 4)
+    {
+        if (array[i] == target)
+        {
             loc = i;
             break;
         }
         i++;
     }
-    if(loc == -1) {
+    if (loc == -1)
+    {
         printf("Element not found in array");
-    }else{
+    }
+    else
+    {
         printf("Found element at %f", loc);
     }
 
