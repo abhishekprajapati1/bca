d82937bc248bcbba886f0f12b868f993e847749f
abhishekprajapati1 <ap0661236@gmail.com>
2024-09-07 10:13:25 +0530

    :party: INIT: initialized classes repository

---
+++ 2nd_sem/problem_solving/class_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+   // Linear search
+   int array[5] = {5, 1, 2, 3, 4};
+   int target = 4; // worst case
+   int loc = -1;
+    int i = 0;
+    while(i <= 4) {
+        if(array[i] == target) {
+            loc = i;
+            break;
+        }
+        i++;
+    }
+    if(loc == -1) {
+        printf("Element not found in array");
+    }else{
+        printf("Found element at %f", loc);
+    }
+
+    return 0;
+}
