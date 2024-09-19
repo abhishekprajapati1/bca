ee76a2648de1cf3eeecd8e3c7f9f533a6c8281d7
abhishekprajapati1 <ap0661236@gmail.com>
2024-09-19 22:37:45 +0530

    :fire: Fact

--- 2nd_sem/problem_solving/factorial.c
+++ 2nd_sem/problem_solving/factorial.c
@@ -1,17 +1,32 @@
 #include<stdio.h>
 
-int main(){
-
-	int n = 5;
-	int i = 1;
+int factorial_iterative(int n){
+    int i = 1;
 	int fact = 1;
 
 	while(i<=n){
 		fact = fact * i;
 		i++;
 	}
-	
-	printf("The factorial of %d is %d", n, fact);
+
+	return fact;
+}
+
+int factorial_recursive(int n){
+
+    printf("getting the number %n", n);
+    return n;
+}
+
+int main(){
+
+	int n = 5;
+
+	// int factorial_with_iterative_method = factorial_iterative(n);
+	// printf("The factorial of %d with iterative method is %d \n", n, factorial_with_iterative_method);
+
+	int factorial_with_recursive_method = factorial_recursive(n);
+	printf("The factorial of %d with recursive method is %d", n, factorial_with_recursive_method);
 
 	return 0;
 }
